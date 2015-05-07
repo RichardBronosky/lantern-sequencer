d058f055965843817b3ace793af678c65f066ee0
Richard Bronosky <richard@bronosky.com>
2015-05-07 11:47:04 -0400

    Whitespace corrections

--- Receiver_test/Receiver_test.ino
+++ Receiver_test/Receiver_test.ino
@@ -1,4 +1,3 @@
-
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -35,17 +34,17 @@ RF24 radio(9,10);
 void setup(void){
 
   Serial.begin(57600);
-//  printf_begin();
-  
+  //  printf_begin();
+
   radio.begin();
   radio.setAutoAck(0);
-//  radio.printDetails();  
+  //  radio.printDetails();
   radio.openReadingPipe(1,pipe);
   radio.startListening();
 
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  
+
   timeout = millis() + 10000;
 
 }
@@ -54,91 +53,91 @@ void loop(void){
   bool done;
   byte n;
   byte i;
-  
+
   strip.setPixelColor(0, strip.Color(255,0,0));
   strip.setPixelColor(1, strip.Color(0,255,0));
   strip.setPixelColor(2, strip.Color(0,0,255));
   strip.setPixelColor(3, strip.Color(0,255,255));
   strip.show();
-  
+
   while(1){
     done = false;
     switch (pattern){
 
       case 0:  // display r, g and b from master
-      while(!done){
-        red = msg[14];
-        green = msg[15];
-        blue = msg[16];
-        strip.setPixelColor(0, strip.Color(red, green, blue));
-        strip.setPixelColor(1, strip.Color(red, green, blue));
-        strip.setPixelColor(2, strip.Color(red, green, blue));
-        strip.setPixelColor(3, strip.Color(red, green, blue));
-        strip.show();
-        //delay(rate);      
-        if(check_radio()){done = process_msg();}   
-        if (millis() > timeout){
-          timeout = millis()+60000;
-          pattern = 2;
-          done = true;
-        }  
-      }
-      break;
- 
-     case 1:  // color rotating around circle
-      n = (addr-1) * 16;
-      while (!done){
-        if (n<16){
-          strip.setPixelColor(0, strip.Color(255,0,0));
-          strip.setPixelColor(1, strip.Color(255,0,0));
-          strip.setPixelColor(2, strip.Color(255,0,0));
-          strip.setPixelColor(3, strip.Color(255,0,0));
-          strip.show();          
-        }
-        else
-        {
-          strip.setPixelColor(0, strip.Color(0,0,255));
-          strip.setPixelColor(1, strip.Color(0,0,255));
-          strip.setPixelColor(2, strip.Color(0,0,255));
-          strip.setPixelColor(3, strip.Color(0,0,255));
+        while(!done){
+          red = msg[14];
+          green = msg[15];
+          blue = msg[16];
+          strip.setPixelColor(0, strip.Color(red, green, blue));
+          strip.setPixelColor(1, strip.Color(red, green, blue));
+          strip.setPixelColor(2, strip.Color(red, green, blue));
+          strip.setPixelColor(3, strip.Color(red, green, blue));
           strip.show();
+          //delay(rate);
+          if(check_radio()){done = process_msg();}
+          if (millis() > timeout){
+            timeout = millis()+60000;
+            pattern = 2;
+            done = true;
+          }
         }
-        n++;
-        if(check_radio()){done = process_msg();}
-        if (millis() > timeout){
-          timeout = millis()+60000;
-          pattern = 2;
-          done = true;
-        }  
-        delay(rate);
-      }
-      break;
+        break;
 
-     case 2:  // rainbow rotating around
-      n = (addr-1) * 16;
-      while (!done){
+      case 1:  // color rotating around circle
+        n = (addr-1) * 16;
+        while (!done){
+          if (n<16){
+            strip.setPixelColor(0, strip.Color(255,0,0));
+            strip.setPixelColor(1, strip.Color(255,0,0));
+            strip.setPixelColor(2, strip.Color(255,0,0));
+            strip.setPixelColor(3, strip.Color(255,0,0));
+            strip.show();
+          }
+          else
+          {
+            strip.setPixelColor(0, strip.Color(0,0,255));
+            strip.setPixelColor(1, strip.Color(0,0,255));
+            strip.setPixelColor(2, strip.Color(0,0,255));
+            strip.setPixelColor(3, strip.Color(0,0,255));
+            strip.show();
+          }
+          n++;
+          if(check_radio()){done = process_msg();}
+          if (millis() > timeout){
+            timeout = millis()+60000;
+            pattern = 2;
+            done = true;
+          }
+          delay(rate);
+        }
+        break;
+
+      case 2:  // rainbow rotating around
+        n = (addr-1) * 16;
+        while (!done){
           for(i=0; i< strip.numPixels(); i++) {
             strip.setPixelColor(i, Wheel((64 + n) & 255));
           }
           strip.show();
           delay(rate);
-        n++;
-        if(check_radio()){done = process_msg();}
-        if (millis() > timeout){
-          timeout = millis()+60000;
-          pattern = 2;
-          done = true;
-        }  
-        delay(rate);
-      }     
-      break;
+          n++;
+          if(check_radio()){done = process_msg();}
+          if (millis() > timeout){
+            timeout = millis()+60000;
+            pattern = 2;
+            done = true;
+          }
+          delay(rate);
+        }
+        break;
 
-     default:
-      pattern = 2;
+      default:
+        pattern = 2;
     }
-  
+
   }
-  
+
 }
 
 // Input a value 0 to 255 to get a color value.
@@ -146,33 +145,33 @@ void loop(void){
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   } else if(WheelPos < 170) {
     WheelPos -= 85;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   } else {
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   }
 }
 
 bool check_radio(void){
   if (radio.available()){
-   timeout = millis() + 10000;
-   if (msg[1] == 0){     
-     return true;
-   }
-   else if (msg[1] == addr){
-     return true;
-   }
-   else
-   {
-     return false; 
-   }
+    timeout = millis() + 10000;
+    if (msg[1] == 0){
+      return true;
+    }
+    else if (msg[1] == addr){
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
   else
   {
-    return false; 
+    return false;
   }
 }
 
@@ -182,17 +181,17 @@ bool process_msg(void){
   switch (cmnd){
     case 'P':    // set pattern number
       if (msg[3] != pattern){  // new pattern
-       pattern = msg[3];
-       return true;
+        pattern = msg[3];
+        return true;
       }
       else{
         return false;
       }
-    break;
+      break;
     case 'S':    // set speed of the pattern
       rate = msg[3];
       return false;
-    break;
+      break;
     default:
       return false;
   }
